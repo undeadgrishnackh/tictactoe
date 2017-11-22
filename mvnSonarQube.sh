040a0dafcdc07edee625a9a0d919dcf91d254588
undeadgrishnackh <undeadgrishnackh@gmail.com>
2017-11-22 01:13:12 +0100

    Structure refactor after docker compose rebuild

--- mvnSonarQube.sh
+++ mvnSonarQube.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn clean install test sonar:sonar -Dsonar.jdbc.url=jdbc:h2:tcp://localhost:32770/sonar -Dsonar.host.url=http://localhost:32770
+mvn clean install test sonar:sonar -Dsonar.jdbc.url=jdbc:h2:tcp://localhost:9000/sonar -Dsonar.host.url=http://localhost:9000
